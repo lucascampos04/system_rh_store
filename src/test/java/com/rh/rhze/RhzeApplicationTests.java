02c7184fca101baa5a337f75d231ef8f380ec1a7
lucascampos04 <camposdlucasoli@gmail.com>
2023-12-14 22:50:46 -0300

    feat: file of test

---
+++ src/test/java/com/rh/rhze/RhzeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rh.rhze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhzeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
