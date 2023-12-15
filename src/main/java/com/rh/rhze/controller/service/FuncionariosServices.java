05bcf1d747c3f970d6b9308f84d656ec6687a034
lucascampos04 <camposdlucasoli@gmail.com>
2023-12-15 00:31:15 -0300

    feature: three new business rules - verification of exist email, telephone and cpf.

--- src/main/java/com/rh/rhze/controller/service/FuncionariosServices.java
+++ src/main/java/com/rh/rhze/controller/service/FuncionariosServices.java
@@ -5,6 +5,7 @@ import com.rh.rhze.model.entity.FuncionariosEntity;
 import com.rh.rhze.model.repository.FuncionariosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,5 +24,48 @@ public class FuncionariosServices {
             throw new RuntimeException("Erro ao exibir todos os usuarios");
         }
     }
+    public FuncionariosDto insertFuncionario(FuncionariosDto funcionariosDto){
+        try {
+            FuncionariosEntity funcionariosEntity = new FuncionariosEntity();
+            funcionariosEntity.setName(funcionariosDto.getName());
+            funcionariosEntity.setOffice(funcionariosDto.getOffice());
+            funcionariosEntity.setSalary(funcionariosDto.getSalary());
+            funcionariosEntity.setCpf(funcionariosDto.getCpf());
+            funcionariosEntity.setEmail(funcionariosDto.getEmail());
+            funcionariosEntity.setTelephone(funcionariosDto.getTelephone());
+
+            if ("caixa".equals(funcionariosDto.getOffice())){
+                funcionariosEntity.setHoursMount(220);
+
+                int overtimeHrs = 1;
+
+                if (overtimeHrs > 0){
+                    funcionariosEntity.setOvertime(overtimeHrs);
+                    funcionariosEntity.setHoursMount(funcionariosEntity.getHoursMount() + overtimeHrs);
+                    float newSalary = funcionariosDto.getSalary() + (overtimeHrs * 100);
+                    funcionariosEntity.setSalary(newSalary);
+                }
+            }
+
+            if (funcionariosDto.getCpf() != null && funcionariosRepository.existsByCpf(funcionariosDto.getCpf())){
+                throw new RuntimeException("CPF JÁ CADASTRADO!!!!");
+            }
+
+            if (funcionariosRepository.existsByEmail(funcionariosDto.getEmail())){
+                throw new RuntimeException("EMAIL JÁ CADASTRADO!!!!");
+            }
+
+            if (funcionariosRepository.existsByTelephone(funcionariosDto.getTelephone())){
+                throw new RuntimeException("TELEFONE JÁ CADASTRADO!!!!");
+            }
+
+            FuncionariosEntity save = funcionariosRepository.save(funcionariosEntity);
+            return FuncionariosDto.fromEntity(save);
+        } catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+
 
 }
