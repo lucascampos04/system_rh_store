28034f7317025bf82dddd130e58a0bb68d86c964
lucascampos04 <camposdlucasoli@gmail.com>
2023-12-15 12:24:16 -0300

    feat: new function that list all employees with the field 'cpf' null

--- src/main/java/com/rh/rhze/model/repository/FuncionariosRepository.java
+++ src/main/java/com/rh/rhze/model/repository/FuncionariosRepository.java
@@ -4,9 +4,13 @@ import com.rh.rhze.model.entity.FuncionariosEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FuncionariosRepository extends JpaRepository<FuncionariosEntity, Long> {
     boolean existsByEmail(String email);
     boolean existsByCpf(String cpf);
     boolean existsByTelephone(String telephone);
+
+    List<FuncionariosEntity> findByCpfIsNull();
 }
