233a1f143c4025ec21aab6faa8122dbb1fdc71c0
lucascampos04 <camposdlucasoli@gmail.com>
2023-12-15 12:28:36 -0300

    feat: new router for delete employees.

--- src/main/java/com/rh/rhze/controller/funcionarios/FuncionariosController.java
+++ src/main/java/com/rh/rhze/controller/funcionarios/FuncionariosController.java
@@ -1,6 +1,7 @@
 package com.rh.rhze.controller.funcionarios;
 
 import com.rh.rhze.controller.service.FuncionariosServices;
+import com.rh.rhze.infra.exception.ValidacaoExcpetion;
 import com.rh.rhze.model.dto.FuncionariosDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,7 +16,7 @@ public class FuncionariosController {
 
     @Autowired
     private FuncionariosServices funcionariosServices;
-    @PostMapping("/inserir/funcionario")
+    @PostMapping("/rh/inserir/funcionario")
     public ResponseEntity<String> inserirFuncionario(@RequestBody FuncionariosDto dto){
         try {
             FuncionariosDto funcionariosDto = funcionariosServices.insertFuncionario(dto);
@@ -23,16 +24,25 @@ public class FuncionariosController {
         } catch (Exception e){
             String errorMessage = e.getMessage();
             System.out.println(errorMessage);
-           return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+           return ResponseEntity.status(HttpStatus.CONFLICT).body(errorMessage); // status 409
         }
     }
-
     @GetMapping("/rh/show/funcionarios")
     public List<FuncionariosDto> getAllFuncionarios(){
         try{
             return funcionariosServices.listAllFuncionarios();
         } catch (Exception e){
             throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/rh/deleted/full/nulls/funcionarios")
+    public ResponseEntity<String> deleteFuncionario(){
+        try {
+            funcionariosServices.deletedFuncionarioID();
+            return ResponseEntity.noContent().build(); // status 204
+        } catch (Exception e){
+            throw new ValidacaoExcpetion("TODOS OS FUNCIONARIOS COM CAMPOS NULOS APAGADOS");
         }
     }
 }
