8b2152eacf57ce74665cd8a568313be83dd0f881
lucascampos04 <camposdlucasoli@gmail.com>
2023-12-16 00:17:35 -0300

    feat : configure route security

---
+++ src/main/java/com/rh/rhze/infra/security/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.rh.rhze.infra.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                )
+                .build();
+    }
+
+}
